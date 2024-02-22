794ed6a6d85f6f802cebac0f445332a6f1dc57a5
CamiloCJ09 <camilo.campaz1@u.icesi.edu.co>
2024-02-22 16:38:40 -0500

    Feat: Vite config with tailwind and nextUI

---
+++ EmailFrontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line no-undef
+const { nextui } = require("@nextui-org/react")
+
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx},",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+}
