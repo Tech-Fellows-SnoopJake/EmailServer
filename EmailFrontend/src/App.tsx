93fa511e18f21352e8a301386156efc3bbb261df
felipe-ra <felipeangel50@gmail.com>
2024-02-23 16:10:10 -0500

    added basic app  routing to components

--- EmailFrontend/src/App.tsx
+++ EmailFrontend/src/App.tsx
@@ -1,12 +1,25 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar';
+import EmailList from './components/EmailList';
+import EmailDetail from './components/EmailDetail';
+import ComposeEmail from './components/ComposeEmail';
+import './styles/global.css';
 
 function App() {
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Router>
+      <Header />
+      <Sidebar />
+      <Routes>
+        <Route path="/inbox" Component={EmailList} />
+        <Route path="/email/:id" Component={EmailDetail} />
+        <Route path="/compose" Component={ComposeEmail} />
+        <Route path="/" Component={EmailList} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
