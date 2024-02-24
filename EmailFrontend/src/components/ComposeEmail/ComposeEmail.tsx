ec2af73e49c58b5f3d1f1567604c689954fd6830
felipe-ra <felipeangel50@gmail.com>
2024-02-23 19:58:34 -0500

    added mail compose functionality

--- EmailFrontend/src/components/ComposeEmail/ComposeEmail.tsx
+++ EmailFrontend/src/components/ComposeEmail/ComposeEmail.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './ComposeEmail.css';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; 
+
+
 
 const ComposeEmail: React.FC = () => {
+  const [to, setTo] = useState('');
+  const [subject, setSubject] = useState('');
+  const [body, setBody] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Submit logic not yet implemented, sorry UwU
+    console.log('Sending email:', { to, subject, body });
+    
+  };
+  console.log("IM SUCCESFULLY INSIDE COMPOSEMAIL start");
   return (
-    <div>
-      <h2>Compose Email</h2>
-      {/* Form for composing email will go here */}
+
+    
+    <div className="compose-email">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={to}
+          onChange={(e) => setTo(e.target.value)}
+          placeholder="To"
+          required
+        />
+        <input
+          type="text"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+          placeholder="Subject"
+          required
+        />
+        <ReactQuill
+          value={body}
+          onChange={setBody}
+          placeholder="Body"
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 };
+
+
 
 export default ComposeEmail;
