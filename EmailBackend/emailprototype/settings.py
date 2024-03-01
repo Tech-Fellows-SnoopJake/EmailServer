eae24f251a72e9b0994bcd51b54be9139af02b41
CamiloCJ09 <camilo.campaz1@u.icesi.edu.co>
2024-02-29 19:18:54 -0500

    Fix: fixing cd commands for deploy

--- EmailBackend/emailprototype/settings.py
+++ EmailBackend/emailprototype/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-ctulx*6&w$rh+t24m0n=twoe(n7wz=&3c2n*ay68b882o4cgx!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '34.227.46.194']
 
 
 # Application definition
