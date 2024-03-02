5bca24cdec1f7f6d4f152a255013611b3105af25
CamiloCJ09 <camilo.campaz1@u.icesi.edu.co>
2024-03-02 01:50:58 -0500

    Feat: Working send and receive emails

--- EmailFrontend/src/__tests__/Sidebar.test.tsx
+++ EmailFrontend/src/__tests__/Sidebar.test.tsx
@@ -17,7 +17,6 @@ describe("Sidebar component", () => {
   
       // Comprueba los enlaces y sus URLs esperadas
       expect(screen.getByText("Compose Email")).toHaveAttribute("href", "/compose");
-      expect(screen.getByText("Drafts")).toHaveAttribute("href", "/drafts");
       // ... otras comprobaciones de enlaces y URLs
     });
     
