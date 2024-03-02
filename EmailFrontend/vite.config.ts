fdf089e64f7e4e3cb9c4e4bfdb8f8dfea963b719
CamiloCJ09 <camilo.campaz1@u.icesi.edu.co>
2024-03-01 19:14:48 -0500

    Fix: fixing dockerfile front

--- EmailFrontend/vite.config.ts
+++ EmailFrontend/vite.config.ts
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      host: "127.0.0.1",
-      port: 80,
-    },
-  },
+  
 })
