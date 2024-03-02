829700dbef021003b2b55359dfd0f510a4251ba3
Felipe-RA <49454068+Felipe-RA@users.noreply.github.com>
2024-03-01 23:27:03 -0500

    sanitized email body

--- EmailFrontend/src/components/EmailDetail/EmailDetail.tsx
+++ EmailFrontend/src/components/EmailDetail/EmailDetail.tsx
@@ -1,8 +1,11 @@
 import { useLocation } from 'react-router-dom';
+import DOMPurify from 'dompurify';
 
 const EmailDetail = () => {
   const location = useLocation();
   const email = location.state?.email; // Access the passed email data
+  const sanitizedBody = DOMPurify.sanitize(email.body);
+
 
   if (!email) {
     return <div className="email-detail-frame border border-gray-300 mx-auto p-4 rounded-md shadow-md max-w-2xl bg-white">Email not found.</div>;
@@ -13,7 +16,10 @@ const EmailDetail = () => {
       <h2 className="mb-4">{email.subject}</h2>
       <p className="mb-2"><strong>From:</strong> {email.sender}</p>
       <p className="mb-2"><strong>Date:</strong> {email.date}</p>
-      <div className="email-body whitespace-pre-wrap">{email.body}</div>
+      <div
+      className="email-body whitespace-pre-wrap"
+      dangerouslySetInnerHTML={{ __html: sanitizedBody }}
+      />
     </div>
   );
 };
