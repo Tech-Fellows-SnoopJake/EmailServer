a0b99470c8146fd5f086e8f39809bf567bbd972f
Maria C Recuero <zeldacamila0913@gmail.com>
2024-03-19 23:48:54 -0500

    changed database engine to postgres

--- EmailBackend/emailprototype/settings.py
+++ EmailBackend/emailprototype/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'emailprototype.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgres',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
