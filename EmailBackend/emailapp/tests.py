377053479a5b5e8d3c8dccab175e5c534aae1305
Maria C Recuero <zeldacamila0913@gmail.com>
2024-03-21 00:41:57 -0500

    Refactor test class name to follow PEP 8 naming conventions

--- EmailBackend/emailapp/tests.py
+++ EmailBackend/emailapp/tests.py
@@ -5,7 +5,7 @@ from .models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
 
-class apiTests(APITestCase):
+class ApiTests(APITestCase):
     def setUp(self):
         # Create sample users and folders
         self.client = APIClient()
