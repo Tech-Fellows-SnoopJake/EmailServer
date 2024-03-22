eac7a046eaaab84b8ddaa3c22564d90318b63421
Valwolfor <diegofelipe.gallo@gmail.com>
2024-03-22 11:32:53 -0700

    prevent access to ./inbox without sign in

--- EmailFrontend/src/components/Login/Login.tsx
+++ EmailFrontend/src/components/Login/Login.tsx
@@ -64,7 +64,6 @@ const Login: React.FC<LoginProps> = ({ onLoginSuccess }) => {
       if (status && status in statusErrorMessages) {
         console.error(statusErrorMessages[status])
       } else {
-        console.log(API_URL)
         console.error("Error de Login")
       }
     }
