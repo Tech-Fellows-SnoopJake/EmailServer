6bca8f81286252a7e70138b642d7a48192cda3d2
Valwolfor <diegofelipe.gallo@gmail.com>
2024-03-22 06:33:04 -0700

    Fixing extention name of constant URL

--- EmailFrontend/src/components/Login/Login.tsx
+++ EmailFrontend/src/components/Login/Login.tsx
@@ -1,7 +1,7 @@
 // src/Login.tsx
 import React, { useState } from "react"
 import { Popover, PopoverTrigger, PopoverContent, Button, Input } from "@nextui-org/react"
-import { API_URL } from "../../utils/constants.ts";
+import { API_URL } from "../../utils/constants";
 
 const statusErrorMessages: Record<number, string> = {
   400: "Error de Login: Solicitud incorrecta",
