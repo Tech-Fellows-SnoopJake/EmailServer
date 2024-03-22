6bbaecd0f891f255c275254c9f9bc6b9dbb16370
Valwolfor <diegofelipe.gallo@gmail.com>
2024-03-22 08:41:41 -0700

    last try... to fix IP var

--- EmailFrontend/jest.setup.ts
+++
@@ -1,3 +0,0 @@
-// jest.setup.ts
-
-process.env.VITE_REACT_API_URL = 'http://18.119.121.232:8000';
--- EmailFrontend/src/utils/constants.ts
+++ EmailFrontend/src/utils/constants.ts
@@ -1,14 +1,21 @@
-// src/config.ts
+// /constants.ts
 
 // Función para obtener variables de entorno de forma segura
 function getEnvVariable(key: string, defaultValue: string): string {
+  // Verifica si está en un entorno de testing (Jest)
+  if (process.env.NODE_ENV === 'test') {
+    // Usa process.env, pues import.meta.env no está disponible en Jest
+    return process.env[key] || defaultValue;
+  }
+
   // Intenta usar import.meta.env si está disponible (para Vite)
+  // Asegúrese de tener la configuración adecuada para esto en su compilación Vite/TypeScript
+  // Este código se presume que está funcionando fuera de Jest, donde import.meta.env es válida
   if (typeof import.meta !== 'undefined' && import.meta.env) {
     return import.meta.env[key] || defaultValue;
   }
 
-  // De lo contrario, usa process.env (compatible con Node.js/Jest)
-  return process.env[key] || defaultValue;
+  return defaultValue;
 }
 
 // Definir variables de entorno específicas
